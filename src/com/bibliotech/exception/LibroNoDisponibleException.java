b88bf3abaa19d7fb5a1ff2fc808506d7081fb4b7
Tobias <62731284+tobiastka@users.noreply.github.com>
2026-04-28 21:04:17 -0300

    Excepciones del sistema

---
+++ src/com/bibliotech/exception/LibroNoDisponibleException.java
@@ -0,0 +1,7 @@
+package com.bibliotech.exception;
+
+public class LibroNoDisponibleException extends BibliotecaException {
+    public LibroNoDisponibleException(String message) {
+        super(message);
+    }
+}
